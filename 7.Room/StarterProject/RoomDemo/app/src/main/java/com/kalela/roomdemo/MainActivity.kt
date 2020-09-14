3d9a2dd8425e48c65e6340c20f3c5af9c93c332c
Kalela <philipkalela@gmail.com>
2020-09-14 16:44:28 +0300

    set up crud operations with Livedata, Room and ViewModel

--- 7.Room/StarterProject/RoomDemo/app/src/main/java/com/kalela/roomdemo/MainActivity.kt
+++ 7.Room/StarterProject/RoomDemo/app/src/main/java/com/kalela/roomdemo/MainActivity.kt
@@ -2,10 +2,47 @@ package com.kalela.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kalela.roomdemo.databinding.ActivityMainBinding
+import db.Subscriber
+import db.SubscriberDatabase
+import db.SubscriberRepository
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+
+        subscriberViewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
+        binding.subscriberViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+        displaySubscribersList()
+    }
+
+    private fun displaySubscribersList() {
+        subscriberViewModel.subscribers.observe(this, Observer {
+            binding.subscriberRecyclerView.adapter = SubscribersRecyclerViewAdapter(it) { selectedItem: Subscriber -> listItemClicked(selectedItem)}
+        })
+    }
+
+    private fun listItemClicked(subscriber: Subscriber) {
+        Toast.makeText(this, "Selected name is ${subscriber.name}", Toast.LENGTH_LONG).show()
+        subscriberViewModel.initUpdateAndDelete(subscriber)
     }
 }
---
+++ 7.Room/StarterProject/RoomDemo/app/src/main/java/com/kalela/roomdemo/SubscriberViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.kalela.roomdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import db.SubscriberRepository
+import java.lang.IllegalArgumentException
+
+class SubscriberViewModelFactory(private val repository: SubscriberRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SubscriberViewModel::class.java)) {
+            return SubscriberViewModel(repository) as T
+        }
+
+        throw IllegalArgumentException("Unknown view model class")
+    }
+}
