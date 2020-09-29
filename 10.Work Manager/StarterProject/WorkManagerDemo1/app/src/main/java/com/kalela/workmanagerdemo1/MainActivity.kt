68db5a7ea5b39832a8e648fa3dd6b6879ecdc291
Kalela <philipkalela@gmail.com>
2020-09-29 14:58:59 +0300

    worker manager one time work request

---
+++ 10.Work Manager/StarterProject/WorkManagerDemo1/app/src/main/java/com/kalela/workmanagerdemo1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.kalela.workmanagerdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        WorkManager.getInstance(applicationContext).enqueue(oneTimeWorkRequest)
+    }
+}
---
+++ 10.Work Manager/StarterProject/WorkManagerDemo1/app/src/main/java/com/kalela/workmanagerdemo1/UploadWorker.kt
@@ -0,0 +1,23 @@
+package com.kalela.workmanagerdemo1
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.lang.Exception
+
+class UploadWorker(context: Context, params:WorkerParameters) : Worker(context, params) { // Pass context and params to constructor of parent class
+    private val TAG = "UploadWorker"
+    override fun doWork(): Result {
+        try {
+            for (i in 0..600) {
+                Log.i(TAG, "Uploading: $i")
+            }
+
+            return Result.success()
+        } catch (e : Exception) {
+            return Result.failure()
+        }
+
+    }
+}
