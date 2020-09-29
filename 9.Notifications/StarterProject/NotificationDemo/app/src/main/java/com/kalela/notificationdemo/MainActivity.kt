868d5d2532c95283e24134488a3b6032e16217c5
Kalela <philipkalela@gmail.com>
2020-09-29 12:34:51 +0300

    action buttons on notifications

--- 9.Notifications/StarterProject/NotificationDemo/app/src/main/java/com/kalela/notificationdemo/MainActivity.kt
+++ 9.Notifications/StarterProject/NotificationDemo/app/src/main/java/com/kalela/notificationdemo/MainActivity.kt
@@ -30,7 +30,26 @@ class MainActivity : AppCompatActivity() {
         val notificationId = 45
 
         val tapResultIntent = Intent(this, SecondActivity::class.java)
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, tapResultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,
+                0,
+                tapResultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+
+        //action button 1
+        val intent2 = Intent(this, DetailsActivity::class.java)
+        val pendingIntent2: PendingIntent = PendingIntent.getActivity(this,
+                0,
+                intent2,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        val action2: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Details", pendingIntent2).build()
+
+        // action button 2
+        val intent3 = Intent(this, SettingsActivity::class.java)
+        val pendingIntent3: PendingIntent = PendingIntent.getActivity(this,
+                0,
+                intent3,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        val action3: NotificationCompat.Action = NotificationCompat.Action.Builder(0, "Settings", pendingIntent3).build()
 
         val notification = NotificationCompat.Builder(this@MainActivity, channelId)
                 .setContentTitle("Demo Title")
@@ -39,6 +58,8 @@ class MainActivity : AppCompatActivity() {
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent)
+                .addAction(action2)
+                .addAction(action3)
                 .build()
         notificationManager?.notify(notificationId, notification)
     }
