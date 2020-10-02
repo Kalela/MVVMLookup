15f415f211621aacf48e88a1ede3cf456151b88e
Kalela <philipkalela@gmail.com>
2020-10-02 10:15:51 +0300

    using dagger 2 modules for inaccessible classes

--- 11.Dagger 2/StarterProject/DIDemo/app/src/main/java/com/anushka/didemo/SmartPhone.kt
+++ 11.Dagger 2/StarterProject/DIDemo/app/src/main/java/com/anushka/didemo/SmartPhone.kt
@@ -3,6 +3,7 @@ package com.anushka.didemo
 import android.util.Log
 import javax.inject.Inject
 
+
 class SmartPhone @Inject constructor(val battery: Battery, val simCard: SIMCard, val memoryCard: MemoryCard) {
 
     init {
