fdfaae874199747d0c8d49320a6e43402e4ab44d
Kalela <philipkalela@gmail.com>
2020-10-02 10:05:16 +0300

    constructor injection with dagger 2

--- 11.Dagger 2/StarterProject/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ 11.Dagger 2/StarterProject/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -4,21 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val battery = Battery();
-        val memoryCard = MemoryCard();
-        val serviceProvider = ServiceProvider();
-        val simCard = SIMCard(serviceProvider);
-        val smartPhone = SmartPhone(battery, simCard, memoryCard)
-        smartPhone.makeACallWithRecording()
+        DaggerSmartPhoneComponent.create()
+            .getSmartPhone()
+            .makeACallWithRecording()
 
-        //Types of dependency injection
-        // 1. Constructor injection(self explanatory)
-        // 2. Method injection (using setters)
-        // 3. Field injection (using public fields in a class)
+//        val battery = Battery();
+//        val memoryCard = MemoryCard();
+//        val serviceProvider = ServiceProvider();
+//        val simCard = SIMCard(serviceProvider);
+//        val smartPhone = SmartPhone(battery, simCard, memoryCard)
+//        smartPhone.makeACallWithRecording()
+
     }
 }
